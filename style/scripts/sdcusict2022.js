f1fb712f5fd357fa605950f3320c347830e8b8fe
Anirudh Gautam <87705614+14anirudh@users.noreply.github.com>
2022-05-27 00:19:26 +0530

    Update sdcusict2022.js

--- style/scripts/sdcusict2022.js
+++ style/scripts/sdcusict2022.js
@@ -1,3 +1,18 @@
+/*footer*/
+const form = document.querySelector("#form");
+
+const mains = document.querySelector(".mains");
+
+const close = document.querySelector(".end");
+
+form.addEventListener("click", function () {
+  mains.classList.add("active");
+});
+close.addEventListener("click", function () {
+  mains.classList.remove("active");
+});
+
+
 // carousel -- 2 start
 const carouselSecondItems = document.querySelectorAll(".carousel--2");
 carouselSecondItems.forEach((item) => {
