8c5798b0855b5f45e8353c2b1dd17680156e679e
Mihir <mihirjha47119@gmail.com>
2022-09-26 22:54:53 +0530

    project page

--- sdcusictprojects.php
+++ sdcusictprojects.php
@@ -18,7 +18,7 @@
 </div>
 
 <!-- Web Team -->
-
+<!-- 
 <div class="container-fluid">
     <div class="row pt-5 pt">
         <div class="col-lg-6 pt-5 sfstCss">
@@ -28,40 +28,214 @@
                 <pre class="projectTxt4">
             Web Development Team works on the 
             official websites under the Software
-            Development Cell, University School Of
-            Information, Communication And Technology.
+            Development Cell, University School of
+            Information, Communication And Technology. about-img
         </pre>
-                <a href="webDevTeam.php" class="secLink">LINK</a>
+                <a href="webDevTeam.php" class="secLink btn btn-1 sty-1">LINK</a> 
             </b>
         </div>
         <div class="col-lg-6 cont2 pt-5">
-            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id="fac">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid imgPad" id='fac'>
-            <div class="pt-sm-5">
-                <img src="harshit.jpeg" alt="" class="sfstImg img-fluid up" id="fac">
-                <img src="shubham.jpg" alt="" class="sfstImg img-fluid imgPad" id='fac'>
-            </div>
+            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id="fac">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
+            <!-- <img src="ipu.jpeg" alt="" class="sfsImg img-fluid imgPad" id='fac'> -->
+            <!-- <div class="card middle">
+                <div class="front">
+                <img src="harshit.jpeg" alt="" class="transit staff-1" >
+                </div>
+
+                <div class="back">
+                    <div class="back-content middle">
+                        <h2>linkedin</h2>
+                        <span>youtube</span>
+                    </div>
+                </div> -->
+
+                <!-- <img src="shubham.jpg" alt="" class="about-img staff-1" > -->
+            <!-- </div> -->
+            <!-- <div class="card middle">
+                <div class="front">
+                <img src="shubham.jpeg" alt="" class="transit staff-1" >
+                </div>
+
+                <div class="back">
+                    <div class="back-content middle">
+                        <h2>linkedin</h2>
+                        <span>youtube</span>
+                    </div>
+                </div> -->
+
+                <!-- <img src="shubham.jpg" alt="" class="about-img staff-1" > -->
+            <!-- </div>
         </div>
     </div>
 </div>
-<div class="box"></div>
+ -->
+<!----img class previous- sfstImg img-fluid imgPad
+id="fac"
 
+class="pt-sm-5"--->
+<!-- <div class="box"></div> 
+
+<style>
+        .about-img {
+            position: relative;
+            border-radius: 20px 20px;
+            border: 5px;
+            border-style: groove;
+            border-width: 2px;
+            border-color: #a1abb4;
+        
+        }
+
+        .about-img:before{
+            position: absolute;
+            right: -30px;
+            top: 30px;
+            border: 10px solid rgba(237, 243, 171);
+            border-radius: 2px 2px 2px 2px;
+            width: 25%;
+            height: 60%;
+        }
+
+        a.sty-1, a.sty-3{
+            margin-left: 65vh;
+        }
+        a.sty-2{
+            margin-right: -8vh
+            ;
+        }
+
+        .staff-1{
+            vertical-align:middle; 
+            width: 30%; 
+            height: 65%; 
+            padding-top: 0px; 
+        }
+        
+       .btn{
+        border: 2px solid #3498db;
+        background: none;
+        padding: 10px 20px;
+        font-size: 20px;
+        font-family: "montserrat";
+        cursor: pointer;
+        margin: 10px;
+        transition: 0.8s;
+        position: center;
+        overflow: hidden;
+       }
+
+       .btn-1{
+        color: #3498db;
+       }
+
+       .btn-1:hover{
+        color: #fff;
+       }
+
+       .btn::before{
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 0%;
+        background: #3498db;
+        z-index: -1;
+        transition: 0.8s;
+       }
+
+       .btn-1::before{
+        top: 0;
+        border-radius: 0 0 50% 50%;
+       }
+
+       .btn-1:hover::before{
+        height: 180%;
+       }
+
+       .about-img1 {
+            position: relative;
+            border-radius: 20px 20px;
+            border: 5px;
+            border-style: groove;
+            border-width: 2px;
+            border-color: #a1abb4;
+            /* margin-left: 7vw; */
+            /* padding-left: 10px; */
+        }
+
+        .container{
+            margin-left: 19vw;
+            /* padding-top: 2px; */
+        }
+
+        .middle{
+            position: absolute;
+            /* top: 50%;
+            left: 50%; */
+            /* transform: translate(-50%, 50%); */
+        }
+        
+        .card{
+            cursor: pointer;
+            /* width: 340px;
+            height: 480px; */
+        }
+
+        .front, .back{
+            width: 20vh;
+            height: 20vh;
+            overflow: hidden;
+            backface-visibility: hidden;
+            /* position: absolute; */
+            transition: transform .6s linear;
+        }
+        .front img{
+            height: 100%;
+            width: 100%;
+        }
+        .front{
+            background: #f1f1f1;
+            transform: perspective(500px) rotateY(0deg);
+        }
+
+        .back{
+            background: #f1f1f1;
+            transform: perspective(500px) rotateY(180deg);
+        }
+        .back-content{
+            color: #2c3e50;
+            text-align: center;
+            width: 100%;
+        }
+        .card:hover > .front{
+            transform: perspective(500px) rotateY(-180deg);
+        }
+        .card:hover > .back{
+            transform: perspective(500px) rotateY(180deg);
+        }
+
+
+
+
+</style>
+ -->
 <!-- SFS TEAM -->
-<div class="container-fluid mt-4">
+
+<!-- <div class="container-fluid mt-4">
     <div class="row">
         <div class="col-lg-6 cont2 pt-5">
-            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id="fac">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid imgPad" id='fac'>
-            <div class="pt-sm-5">
-                <img src="harshit.jpeg" alt="" class="sfstImg img-fluid up" id="fac">
-                <img src="shubham.jpg" alt="" class="sfstImg img-fluid imgPad" id='fac'>
+            <img src="ipu.jpeg" alt="" class="sfsImg1 img-fluid" id="fac">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
+            <!-- <img src="ipu.jpeg" alt="" class="sfsImg img-fluid imgPad" id='fac'> -->
+            <!-- <div class="pt-sm-5 container">
+                <img src="harshit.jpeg" alt="" class="about-img1 staff-1" >
+                <img src="shubham.jpg" alt="" class="about-img1 staff-1" >
             </div>
         </div>
         <div class="col-lg-6 sfstCss2 order-2 order-md-1">
-            <img src="ic1.png" alt="ic1" class="icn1 img-fluid"><br><br>
-            <h2 class="ps-sm-5  projectTxt3">STUDENT FEEDBACK SYSTEM TEAM</h2><br>
+            <img src="ic1.png" alt="ic1" class="icn2 img-fluid"><br><br>
+            <h2 class="ps-sm-5  projectTxt8">STUDENT FEEDBACK SYSTEM TEAM</h2><br>
             <b>
-                <pre class="projectTxt4 sfsTxt d-flex justify-content-center">
+                <pre class="projectTxt5 ">
         Student Feedback System(SFS) portals are 
         solely for the purpose of receiving feedback
         from students of all programs for their respective
@@ -70,15 +244,16 @@
         each semester in all USS's of GGSIPU.
             </pre>
             </b>
-            <a href="SFSTeam.php" class="secLink">LINK</a>
+            <a href="SFSTeam.php" class="secLink btn btn-1 sty-2">LINK</a>
         </div>
     </div>
-</div>
-<div class="box1"></div> <!-- Bottom curved line -->
+</div> -->
+<!-- <div class="box1"></div> <!- Bottom curved line  -->
 
-<!-- App Team -->
 
-<div class="container-fluid">
+<!-- App Team --> 
+ 
+<!-- <div class="container-fluid">
     <div class="row pt-5 pt">
         <div class="col-lg-6 pt-5 sfstCss">
             <img src="ic1.png" alt="ic1" class="icn1 img-fluid"><br><br>
@@ -91,19 +266,86 @@
             Information, Communication And Technology.
             </pre>
             </b>
-            <a href="appDevTeam.php" class="secLink">LINK</a>
+            <a href="appDevTeam.php" class="secLink btn btn-1 sty-3">LINK</a>
 
         </div>
+
+
         <div class="col-lg-6 cont2 pt-5">
             <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id="fac">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id='fac'>
-            <div class="pt-sm-5">
-                <img src="harshit.jpeg" alt="" class="sfstImg img-fluid up" id="fac">
-                <img src="shubham.jpg" alt="" class="sfstImg img-fluid imgPad" id='fac'>
+            < <img src="ipu.jpeg" alt="" class="sfsImg img-fluid" id='fac'> -->
+            <!-- <div class="pt-sm-5">
+                <img src="harshit.jpeg" alt="" class="about-img staff-1" >
+                <img src="shubham.jpg" alt="" class="about-img staff-1" >
             </div>
         </div>
     </div>
 </div>
+ --> 
+
+<!--------------------------------NEW DESIGN----------------------------->
+
+<!-- <div class="new-vert " id="vert">
+    <div class="container">
+    <h2 >WEB DEVELOPMENT TEAM </h2>
+    <p id="web-d">  <b> Web Development Team works on the 
+            official websites under the Software
+            Development Cell, University School Of
+            Information, Communication And Technology.</b>
+    
+    </div>
+    <img src="light.jpg" id="img-light">
+</p>
+</div>
+
+<style>
+
+
+    .new-vert{
+        height:200vh;
+        background: repeating-linear-gradient(90deg, #fffcfc 0%, #fffcfc 5%, #e2f6fe 5%, #e2f6fe 10%);
+
+    }
+    /* .container{
+        width: 500px;
+        height: 100px;
+        padding-left: 50px;
+        background: #000000;
+    
+    }
+     */
+
+     .container{
+        position: relative;
+        margin-top: 0px;
+        padding-left: 50px;
+        padding-top: 120px;
+        justify-content: space-between;
+        /* display: flex; */
+     }
+     .container h2{
+        font-family: arial;
+        font-size: 2em;
+        font-color: #00000 !important;
+     }
+     img{
+        margin-left: 100px;
+     }
+     /* #web-d{
+        color: #000;
+     } */
+     p{
+        width: 600px;
+        height: 400px;
+        text-align: left;
+        margin-top: 38px;
+        color: #000 !important;
+     }
+</style>
+ -->
+
+
+
 
 <!-- <div class="container-fluid pt-5">
         <div class="row">
@@ -128,4 +370,228 @@
             </div>
         </div>
     </div> -->
+
+<!----------------------------------------NEW 2nd DESIGN----------------------------------->
+<body>
+    
+
+    <div class="container1" style="margin-left: 0px; margin-right: 0px; margin-bottom: 20px; margin-top: 20px">
+        <div class="rectangle"></div>
+        <div>
+            <img class="icon1" src="ic1.png" alt="">
+        </div>
+        <div>
+            <h2 class="head1" style="position: absolute; top: 30%;">student FEEDBACK SYSTEM TEAm</h2>
+            <div class="para1">
+            Faculty Feedback System(FFS) portals are solely for the purpose of receiving feedback from students of all programs for their respective faculties allotted during a particular semester. This procedure takes place towards the end of each semester in all USS's of GGSIPU.
+            </div>
+        <div>
+        <button href="webDevTeam.php" class="container2 btnn-1" style="position: absolute; top: 79%; left: 7%;">View Team</button>
+        </div>
+        <div class="leads">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+        </div>
+        </div>
+
+        
+    </div>
+    <div class="container1" style="margin-left: 0px; margin-right: 0px; margin-bottom: 20px; margin-top: 10vw">
+        <div class="rectangle"></div>
+        <div>
+            <img class="icon1" src="web1.png" alt="" style="left: 75%;">
+        </div>
+        <div>
+            <h2 class="head1" style="position: absolute; top: 30%; left:55%; margin-left: 8vw;">web development TEAM</h2>
+            <div class="para1" style="text-align: right;
+    margin-left: 23vw; padding-top: 1.2vw">
+            Web Development Team works on the 
+            official websites under the Software
+            Development Cell, University School Of
+            Information, Communication And Technology.
+            </div>
+        <div>
+        <button href="webDevTeam.php" class="container2 btnn-1" style="position: absolute; top: 79%; left: 82.6vw;">View Team</button>
+        </div>
+        <div class="leads" style="left: 13vw;">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+        </div>
+        </div>
+
+        
+    </div>
+    <div class="container1" style="margin-left: 0px; margin-right: 0px; margin-bottom: 20px; margin-top: 10vw">
+        <div class="rectangle"></div>
+        <div>
+            <img class="icon1" src="app1.png" alt="">
+        </div>
+        <div>
+            <h2 class="head1" style="position: absolute; top: 30%;">App development team</h2>
+            <div class="para1">
+            Web Development Team works on the 
+            official websites under the Software
+            Development Cell, University School Of
+            Information, Communication And Technology.
+            </div>
+        <div>
+        <button href="webDevTeam.php" class="container2 btnn-1" style="position: absolute; top: 79%; left: 7%;">View Team</button>
+        </div>
+        <div class="leads">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+            <img class="lead1" src="rahulsir.jpg" alt="">
+        </div>
+        </div>
+
+        
+    </div>
+
+
+
+</body>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+
+.lead1{
+    width: 50px;
+}
+
+.container1{
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    height: 30vw;  
+    background: #E2F6FE;
+    padding-left: 0px;
+    border-radius: 48% 53% 0% 0% / 5% 5% 0% 0% ;
+}
+
+
+.icon1{
+    width: 5%;
+    height: 15%;
+    position: absolute;
+    left: 20%;
+    top: 10%;   
+}
+.head1{
+    font-family: 'Roboto', sans-serif;
+    color: black;
+    text-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
+    font-size: 2.3vw;
+    line-height: 42px;
+    text-transform: uppercase;
+    left: 7%;
+}
+    .para1{
+    color: #000;
+    position: absolute;
+    line-height: 39px;
+    width: 70%;
+    padding-left: 7%;
+    top: 40%;
+    font-weight: 500;
+    font-size: 2vw;
+
+
+}
+    /* .btn{
+        border: 2px solid #3498db;
+        background: none;
+        padding: 10px 20px;
+        font-size: 20px;
+        font-family: "montserrat";
+        cursor: pointer;
+        margin: 10px;
+        transition: 0.8s;
+        position: center;
+        overflow: hidden;
+       }
+
+       .btn-1{
+        color: #3498db;
+       }
+
+       .btn-1:hover{
+        color: #fff;
+       }
+
+       .btn::before{
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 0%;
+        background: #3498db;
+        z-index: -1;
+        transition: 0.8s;
+       }
+
+       .btn-1::before{
+        top: 0;
+        border-radius: 0 0 50% 50%;
+       }
+
+       .btn-1:hover::before{
+        height: 180%;
+       } */
+       .container2{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        text-align: center;
+        min-height: 7vh;
+
+       }
+       .btnn-1{
+        width: 10%;
+        height: 10%;
+        max-height: 15%;
+        border: none;
+        color: white;
+        background-color: #041D53;
+        border-radius: 4px;
+        box-shadow: inset 0 0 0 0 #E1E3E7;
+        transition: ease-out 0.8s;
+        font-size: 130%;
+        outline: none;
+       }
+       .btnn-1:hover{
+        box-shadow: inset 300px 0 0 0 #E1E3E7;
+        cursor: pointer;
+        color: #000;
+
+       }
+       .leads{
+        display: flex;
+        align-items: center;
+        position: absolute;
+        height: 0%;
+        left: 50%;
+        top: 100%;
+       }
+
+       .lead1{
+        height: 11vw;
+        width: 11vw;
+        border-radius: 50%;
+        border: 3px solid #000;
+        filter: drop-shadow(0px 4px 144px rgba(0, 0, 0, 0.25));
+        padding: 2px;
+        margin: 1.5vw;
+       }
+</style>
+
+
+
+
+    
 <?php include 'sdcusict2022footer.php' ?>
