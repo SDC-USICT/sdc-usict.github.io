3ddf9e7667881be187f47c06c5ef9e5545c83c42
ManuKashyap01 <kashyap.manu200201@gmail.com>
2022-09-30 11:30:48 +0530

    SDC student profile page

---
+++ sdcstudent.php
@@ -0,0 +1,128 @@
+<?php include 'sdcusict2022header.php' ?>
+  <!-- carousel -->
+  <?php include 'sdcusict2022carousal.php' ?>
+
+  <style>
+    ::-webkit-scrollbar-thumb{
+      width: 10px;
+      background-color: #041d53;
+      border-radius: 10px;
+    }
+  </style>
+
+    <div class="container__flex">
+      <div class="profile__div">
+        <div class="profile__photo">
+          <img class="profile__img" src="./shubham.jpg" alt="" />
+        </div>
+      </div>
+      <div class="social__div">
+        <div class="social__heading">
+          <h3 class="heading__name">Anurag Parashar</h3>
+          <p class="heading__desg">Lead</p>
+        </div>
+        <div class="social__icons">
+          <a href="#" class="social__icon">
+            <img src="./linkedin-circle.png" alt="" />
+          </a>
+          <a href="#" class="social__icon">
+            <img src="./github-circle.png" alt="" />
+          </a>
+          <a href="#" class="social__icon">
+            <img src="./mail-circle.png" alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="myself__div">
+        <div class="myself__divinside">
+          <h1 class="myself__heading">About Myself</h1>
+          <p class="myself__content black__color">
+            I am Anurag Parashar, a Full Stack Developer and a Content Writer
+            with a knack for creativity. I am 3rd year CSE student at University
+            School Of Information and Communication Technology, Guru Gobind
+            Singh Indraprastha University, Dwarka, Delhi, India. I am one of the
+            Lead of the Software Development Cell.
+          </p>
+          <button class="button myself__button">View Team</button>
+        </div>
+      </div>
+      <div class="project__div">
+        <h1 class="project__heading">Projects</h1>
+        <div class="project__cards">
+          <div class="project__card">
+            <h3 class="project__title black__color">Personal Website</h3>
+            <p style="text-align: left;" class="project__content black__color">
+              A portfolio website which provides online representation of my
+              work, as well as my skills and experiences.
+            </p>
+            <button class="button project__button">See Projects</button>
+          </div>
+          <div class="project__card">
+            <h3 class="project__title black__color">Personal Website</h3>
+            <p style="text-align: left;" class="project__content black__color">
+              A portfolio website which provides online representation of my
+              work, as well as my skills and experiences.
+            </p>
+            <button class="button project__button">See Projects</button>
+          </div>
+          <div class="project__card">
+            <h3 class="project__title black__color">Personal Website</h3>
+            <p style="text-align: left;" class="project__content black__color">
+              A portfolio website which provides online representation of my
+              work, as well as my skills and experiences.
+            </p>
+            <button class="button project__button">See Projects</button>
+          </div>
+        </div>
+      </div>
+      <div class="skill__div">
+        <h1 class="skill__heading">Skills</h1>
+        <div class="skill__cards">
+          <div class="skill__card">
+            <div class="skillCard__img">
+              <img src="./web-design 1.png" alt="" />
+            </div>
+            <h2 class="skillCard__heading">Web Development</h2>
+            <p class="black__color skillCard__content">
+              HTML5, CSS3, JavaScript(ES5, ES6), JSON, AJAX, ReactJS, NodeJS,
+              JQuery, API, Bootstrap4, Materialize, MySQL, Git, GItHub, Heroku,
+              Firebase
+            </p>
+          </div>
+          <div class="skill__card">
+            <div class="skillCard__img">
+              <img src="./web-design 1.png" alt="" />
+            </div>
+            <h2 class="skillCard__heading">Web Development</h2>
+            <p class="black__color skillCard__content">
+              Bootstrap4, Materialize, Photoshop and UserTesting.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="exp__div">
+        <h1 class="exp__heading">Experiences</h1>
+        <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading">ACM USICT STUDENT CHAPTER</h2>
+            <p class="black__color exp__time">November 2021 – Present</p>
+          </div>
+          <p class="black__color exp__position">Membership Chair</p>
+        </div>
+        <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading">ACM USICT STUDENT CHAPTER</h2>
+            <p class="black__color exp__time">November 2021 – Present</p>
+          </div>
+          <p class="black__color exp__position">Membership Chair</p>
+        </div>
+        <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading">ACM USICT STUDENT CHAPTER</h2>
+            <p class="black__color exp__time">November 2021 – Present</p>
+          </div>
+          <p class="black__color exp__position">Membership Chair</p>
+        </div>
+      </div>
+    </div>
+  <?php include 'sdcusict2022footer.php' ?>
