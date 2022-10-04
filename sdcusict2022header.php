12038957b9fa27c6c75d7a077f2463c93073d494
Divyansh3021 <divyansh3021@gmail.com>
2022-10-04 19:10:34 +0530

    Navbar fixed

--- sdcusict2022header.php
+++ sdcusict2022header.php
@@ -28,7 +28,7 @@
 
     <div class="collapse navbar-collapse me-auto" style="border-radius: 0 0 15% 0" id="navbarSupportedContent">
       <ul class="navbar-nav ms-2 mb-2 mb-lg-0 mt-5 me-lg-5 d-lg-none">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="sdcusict.php">
             <svg class="mb-1 ms-2" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
               class="bi bi-house-door text-white" viewBox="0 0 16 16">
@@ -37,7 +37,7 @@
             </svg>
             Home
           </a>
-        </li>
+        </li> -->
 
         <!-- <li class="nav-item dropdown mx-lg-3">
           <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
@@ -95,7 +95,68 @@
           Home
         </a>
       </li>
+      <li class="nav-item mx-lg-3 hover">
+        <a class="nav-link active d-inline" aria-current="page" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-house-door text-white" viewBox="0 0 16 16">
+            <path
+            d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zm3.515 7.008L14.438 10 8 13.433 1.562 10 4.25 8.567l3.515 1.874a.5.5 0 0 0 .47 0l3.515-1.874zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z" />
+          </svg>
+        </a>
+        <a class="nav-link active hover-text mt-auto" aria-current="page" href="sdcusict.php">
+          Projects
+        </a>
+      </li>
+      <!-- <li class="nav-item mx-lg-3 hover">
+        <a class="nav-link active d-inline" aria-current="page" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-house-door text-white" viewBox="0 0 16 16">
+            <path
+            d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+          </svg>
+        </a>
+        <a class="nav-link active hover-text mt-auto" aria-current="page" href="sdcusict.php">
+          Teams
+        </a>
+      </li> -->
+      <li class="nav-item dropdown mx-lg-3 hover" id="lg-dropdown">
+        <a class="nav-link text-white d-inline" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-people text-white" viewBox="0 0 16 16">
+            <path
+              d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+          </svg>
+        </a> 
 
+        <ul class="dropdown-menu mt-0" id="lg-dropdown-menu" aria-labelledby="navbarDropdown">
+          <li>
+            <a class=" text-white mt-2 lg-dropdown-item" href="sdcusictfaculty.php">Faculty</a>
+          </li>
+          <li>
+            <a class=" text-white lg-dropdown-item" href="#core-team">Core Team</a>
+          </li>
+        </ul>
+
+        <a class="nav-link active hover-text" aria-current="page" href="#">
+          Teams
+        </a>
+      </li>
+      <!--  -->
+      <li class="nav-item mx-lg-3 hover">
+        <a class="nav-link active d-inline" aria-current="page" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+            class="bi bi-house-door text-white" viewBox="0 0 16 16">
+            <path
+            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+          </svg>
+        </a>
+        <a class="nav-link active hover-text mt-auto" aria-current="page" href="sdcusict.php">
+          Contact Us
+        </a>
+      </li>
+
+      
       <!-- <li class="nav-item dropdown mx-lg-3 hover" id="lg-dropdown">
         <a class="nav-link text-white d-inline" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
           aria-expanded="false">
