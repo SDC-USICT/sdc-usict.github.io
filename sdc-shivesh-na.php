0c9ad9f910e41e05bd0174889def1e270f2244d6
ManuKashyap01 <kashyap.manu200201@gmail.com>
2022-10-07 10:49:28 +0530

    added student profile pages

---
+++ sdc-shivesh-na.php
@@ -0,0 +1,112 @@
+<?php include 'sdcusict2022header.php' ?>
+  <!-- carousel -->
+  <?php include 'sdcusict2022carousal.php' ?>
+
+  <style>
+    ::-webkit-scrollbar-thumb{
+      width: 10px;
+      background-color: #041d53;
+      border-radius: 10px;
+    }
+  </style>
+
+    <div class="container__flex">
+      <div class="profile__div shivesh">
+        <div class="profile__photo">
+          <img class="profile__img" src="./students-img/shivesh-nandan.jpg" alt="" />
+        </div>
+      </div>
+      <div class="social__div">
+        <div class="social__heading">
+          <h3 class="heading__name team-heading">Shivesh Nandan</h3>
+          <p class="heading__desg">Developer</p>
+        </div>
+        <div class="social__icons">
+          <a href="https://www.linkedin.com/in/shivesh-nandan-39a1b219a/" target="_blank" class="social__icon">
+            <img src="./linkedin-circle.png" alt="" />
+          </a>
+          <a href="https://github.com/ShiveshNandan" target="_blank" class="social__icon">
+            <img src="./github-circle.png" alt="" />
+          </a>
+          <a href="mailto:kshivesh929@gmail.com" target="_blank" class="social__icon">
+            <img src="./mail-circle.png" alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="myself__div">
+        <div class="myself__divinside">
+          <h1 class="myself__heading team-heading">About Myself</h1>
+          <p class="myself__content black__color">
+            I am currently a frontend web developer. And with time, turning towards the full stack web developer. After completing this, I will start learning web3.
+        </p>
+          <button class="button myself__button"><a href="./webDevTeam.php">View Team</a></button>
+        </div>
+      </div>
+      <div class="project__div">
+        <h1 class="project__heading team-heading">Projects</h1>
+        <div class="project__cards">
+          <div class="project__card">
+            <h3 class="project__title black__color">Student Profile</h3>
+            <p style="text-align: left;" class="project__content black__color">
+            This project is a student profile that will be added to the Ipu website in the student corner.
+            </p>
+            <button class="button project__button"><a href="https://shiveshnandan.github.io/SDC_task/" target="_blank">See Projects</a></button>
+          </div>
+          <div class="project__card">
+            <h3 class="project__title black__color">Hotstar clone</h3>
+            <p style="text-align: left;" class="project__content black__color">
+            this is a clone website of hotstar using HTML, CSS and Javascript.
+            </p>
+            <button class="button project__button"><a href="https://shiveshnandan.github.io/Hotstar-clone/" target="_blank">See Projects</a></button>
+          </div>
+        </div>
+      </div>
+      <div class="skill__div">
+        <h1 class="skill__heading team-heading">Skills</h1>
+        <div class="skill__cards">
+          <div class="skill__card">
+            <div class="skillCard__img">
+              <img src="./web1.png" alt="" />
+            </div>
+            <h2 class="skillCard__heading">Web Development</h2>
+            <p class="black__color skillCard__content">
+                HTML, CSS, JS, react, node.js, express.js
+            </p>
+          </div>
+          <!-- <div class="skill__card" style="width: 400px;">
+            <div class="skillCard__img">
+              <img src="./web2.png" alt="" />
+            </div>
+            <h2 class="skillCard__heading">Web Design</h2>
+            <p class="black__color skillCard__content">
+              Figma, AdobeXD, Canva
+            </p>
+          </div> -->
+        </div>
+      </div>
+      <div class="exp__div container">
+        <h1 class="exp__heading team-heading my-4">Experiences</h1>
+        <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading team-heading">SOFTWARE DEVELOPMENT CELL USICT</h2>
+            <p class="black__color exp__time">May 2022 – Present</p>
+          </div>
+          <p class="black__color exp__position">Web Developer</p>
+        </div>
+        <!-- <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading team-heading">The Spark Foundation</h2>
+            <p class="black__color exp__time">September 2022 – October 2022</p>
+          </div>
+          <p class="black__color exp__position">Web developer</p>
+        </div> -->
+        <!-- <div class="exp__details">
+          <div class="exp__detail">
+            <h2 class="exp__heading team-heading">ACM USICT STUDENT CHAPTER</h2>
+            <p class="black__color exp__time">November 2021 – Present</p>
+          </div>
+          <p class="black__color exp__position">Membership Chair</p>
+        </div> -->
+      </div>
+    </div>
+  <?php include 'sdcusict2022footer.php' ?>
