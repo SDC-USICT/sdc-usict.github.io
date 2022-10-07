c168998cdf7a6aa21a91115f8cc4e2a533521536
SUBHANGI-DHASMANA <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2022-10-07 10:10:22 +0530

    Added contact page

---
+++ sdcusictcontact.php
@@ -0,0 +1,52 @@
+<?php include 'sdcusict2022header.php'?>
+<!-- carousel -->
+<?php include 'sdcusict2022carousal.php'?>
+
+<style>
+    ::-webkit-scrollbar-thumb {
+        width: 10px;
+        background-color: #041d53;
+        border-radius: 10px;
+    }
+</style>
+
+<div class="contact_head">
+    <h1 class="contacts">Lets get in touch</h1>
+</div>
+
+<div class="container">
+    <div class="contact_cards container row m-5">
+        <div class="contact_grid col-md-6">
+            <div class="contact_card">
+                <img src="mail.png" alt="" class="contact-img">
+                <h2 class="contacts">Mail Us</h2>
+                <p class="contacts">sdcusict@ipu.ac.in</p>
+            </div>
+        </div>
+        <div class="contact_grid col-md-6">
+            <div class="contact_card">
+                <img src="twitter.png" alt="" class="contact-img">
+                <h2 class="contacts">Twitter</h2>
+                <p class="contacts">twitter.com/SDC_USICT</p>
+            </div>
+        </div>
+        <div class="contact_grid col-md-6">
+            <div class="contact_card">
+                <img src="linkedin.png" style="width: 50px;" alt="" class="contact-img">
+                <h2 class="contacts">LinkedIn</h2>
+                <p class="contacts">linkedin.com/company/sdc-usict/</p>
+            </div>
+        </div>
+        <div class="contact_grid col-md-6">
+            <div class="contact_card">
+                <img src="inst.png" alt="" class="contact-img">
+                <h2 class="contacts">Instagram</h2>
+                <p class="contacts">instagram.com/sdcusict/</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include 'sdcusict2022footer.php'?>
