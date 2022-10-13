f3c92bb00511c4bd24fa729353b67bfe08dfeca9
SUBHANGI-DHASMANA <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2022-10-13 12:58:29 +0530

    Added links

--- contactus.php
+++ contactus.php
@@ -8,6 +8,19 @@
         background-color: #041d53;
         border-radius: 10px;
     }
+
+    .contact-link{
+        text-decoration: none;
+    }
+
+    .contact-link .contacts{
+        color: #73777B;
+    }
+
+    .contact-link .contacts:hover{
+        transition: all 0.5s ease-in-out;
+        color: #413F42;
+    }
 </style>
 
 <div class="contact_head">
@@ -20,28 +33,28 @@
             <div class="contact_card">
                 <img src="mail.png" alt="" class="contact-img">
                 <h2 class="contacts">Mail Us</h2>
-                <p class="contacts">sdcusict@ipu.ac.in</p>
+                <a href="mailto:sdcusict@ipu.ac.in" class="contact-link" target="_blank"><p class="contacts">sdcusict@ipu.ac.in</p></a>
             </div>
         </div>
         <div class="contact_grid col-md-6">
             <div class="contact_card">
                 <img src="twitter.png" alt="" class="contact-img">
                 <h2 class="contacts">Twitter</h2>
-                <p class="contacts">twitter.com/SDC_USICT</p>
+                <a href="https://twitter.com/SDC_USICT" class="contact-link" target="_blank"><p class="contacts">twitter.com/SDC_USICT</p></a>
             </div>
         </div>
         <div class="contact_grid col-md-6">
             <div class="contact_card">
                 <img src="linkedin.png" style="width: 50px;" alt="" class="contact-img">
                 <h2 class="contacts">LinkedIn</h2>
-                <p class="contacts">linkedin.com/company/sdc-usict/</p>
+                <a href="https://linkedin.com/company/sdc-usict/" class="contact-link" target="_blank"><p class="contacts">linkedin.com/company/sdc-usict/</p></a>
             </div>
         </div>
         <div class="contact_grid col-md-6">
             <div class="contact_card">
                 <img src="inst.png" alt="" class="contact-img">
                 <h2 class="contacts">Instagram</h2>
-                <p class="contacts">instagram.com/sdcusict/</p>
+                <a href="https://instagram.com/sdcusict/" class="contact-link" target="_blank"><p class="contacts">instagram.com/sdcusict/</p></a>
             </div>
         </div>
     </div>
