429f017d3175aac13ba89157cfcc490b8b1c3078
SUBHANGI-DHASMANA <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2022-10-15 23:42:09 +0530

    Added datails

--- webDevTeam.php
+++ webDevTeam.php
@@ -1,6 +1,11 @@
-
-
 <?php include 'sdcusict2022header.php' ?>
+<?php include 'sdcusict2022carousal.php'?>
+
+<style>
+  .our-team{
+    width: 100%;
+  }
+</style>
 
 <body>
 <div style="margin-top: 120px">
@@ -14,87 +19,32 @@
       <div class="trapezium f">
         <p class="heading-card">Team Lead</p>
       </div>
-      <div class="row" style="margin: 2%">
-        <div class="col-md-4 col-sm-6">
-          <div class="our-team">
-            <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+      <div style="margin: 2%">
+        <div class="d-flex justify-content-center">
+          <div class="our-team flex-column" style="width: 25%;">
+            <div class="pic" style="height: auto;">
+              <img src="students-img/shivesh-nandan.jpg" />
             </div>
-            <h3 class="title">Himesh Nayak</h3>
+            <h3 class="title">Shivesh Nandan</h3>
             <span class="post">LEAD, SDC USICT</span>
             <ul class="social">
               <li>
-                <a href="https://github.com/HimeshNayak" target="_blank">
+                <a href="https://github.com/ShiveshNandan" target="_blank">
                   <img src="github-circle.png" class="img-c">
                 </a>
               </li>
               <li>
-                <a href="https://linkedin.com/in/himeshnayak015" target="_blank">
+                <a href="https://www.linkedin.com/in/shivesh-nandan-39a1b219a/" target="_blank">
                   <img class="img-c" src="linkedin-circle.png">
                 </a>
               </li>
               <li>
-                <a href="mailto:himeshnayak015@gmail.com" target="blank">
-                  <img src="mail-circle.png" class="img-c">
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/Himesh_nayak" target="blank">
-                  <img src="twitter-circle.png" class="img-c">
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <div class="our-team">
-            <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
-            </div>
-            <h3 class="title">Shubham Goyal</h3>
-            <span class="post">Fullstack Developer</span>
-            <ul class="social">
-              <li>
-                <a href="https://github.com/ShubhamGoyal7125/" target="_blank">
-                  <img src="github-circle.png" class="img-c">
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/shubham-goyal-7987461b8" target="_blank">
-                  <img class="img-c" src="linkedin-circle.png">
-                </a>
-              </li>
-              <li>
-                <a href="mailto:shubhamgoyal7125@gmail.com" target="_blank">
-                  <img class="img-c" src="mail-circle.png">
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6">
-          <div class="our-team">
-            <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
-            </div>
-            <h3 class="title">Jatin Singh</h3>
-            <span class="post">Fullstack Developer</span>
-            <ul class="social">
-              <li>
-                <a href="https://github.com/Jasngh004" target="_blank">
-                  <img src="github-circle.png" class="img-c">
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/jatin-singh04/" target="_blank"><img src="linkedin-circle.png"
-                    class="img-c"></a>
-              </li>
-              <li>
-                <a href="mailto:jatin2002singh@gmail.com" target="_blank">
+                <a href="mailto:kshivesh929@gmail.com" target="blank">
                   <img src="mail-circle.png" class="img-c">
                 </a>
               </li>
             </ul>
+            <a href="./shivesh-nandan.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
       </div>
@@ -103,13 +53,12 @@
         <p class="heading-card">Team</p>
       </div>
       <div class="row justify-content-center" style="margin: 2%">
-        <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+        <!-- <div class="col-md-3 col-sm-6">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/" />
             </div>
-            <h3 class="title">manu kashyap</h3>
-            <a href="" class="link">project link</a>
+            <h3 class="title">Manu kashyap</h3>
             <ul class="social">
               <li>
                 <a href="https://github.com/HimeshNayak" target="_blank">
@@ -127,11 +76,12 @@
                 </a>
               </li>
             </ul>
+            <a href="" class="view-profile-link my-2">View profile</a>
           </div>
-        </div>
+        </div> -->
 
-        <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+        <!-- <div class="col-md-3 col-sm-6">
+          <div class="our-team flex-column">
             <div class="pic">
               <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
             </div>
@@ -155,39 +105,38 @@
               </li>
             </ul>
           </div>
-        </div>
+        </div> -->
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/tanishq-sharma.jpeg" />
             </div>
             <h3 class="title">tanishq sharma</h3>
-            <a href="" class="link">project link</a>
             <ul class="social">
               <li>
-                <a href="https://github.com/Jasngh004" target="_blank">
+                <a href="https://github.com/sharma-tanishq" target="_blank">
                   <img src="github-circle.png" class="img-c">
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/jatin-singh04/" target="_blank"><img src="linkedin-circle.png"
+                <a href="https://www.linkedin.com/in/tanishq-sharma-aa2639212" target="_blank"><img src="linkedin-circle.png"
                     class="img-c"></a>
               </li>
               <li>
-                <a href="mailto:jatin2002singh@gmail.com" target="_blank">
+                <a href="mailto:sharma.tanishq9999@gmail.com" target="_blank">
                   <img src="mail-circle.png" class="img-c">
                 </a>
               </li>
             </ul>
+            <a href="./tanishq-sharma.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/mihir-jha.jpg" />
             </div>
             <h3 class="title">mihir jha</h3>
-            <a href="" class="link">project link</a>
             <ul class="social">
               <li>
                 <a href="https://github.com/M-J-7" target="_blank">
@@ -205,10 +154,11 @@
                 </a>
               </li>
             </ul>
+            <a href="./mihir-jha.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+        <!-- <div class="col-md-3 col-sm-6">
+          <div class="our-team flex-column">
             <div class="pic">
               <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
             </div>
@@ -232,61 +182,61 @@
               </li>
             </ul>
           </div>
-        </div>
+        </div> -->
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/Gauraverma.png" />
             </div>
             <h3 class="title">gaurav verma</h3>
-            <a href="" class="link">project link</a>
             <ul class="social">
               <li>
-                <a target="_blank" href="https://github.com/sinha-toyeesh">
+                <a target="_blank" href="https://github.com/abcdeCoder">
                   <img src="github-circle.png" class="img-c">
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/toyeesh-sinha/" target="_blank">
+                <a href="https://www.linkedin.com/in/gaurav-verma-88056a224/" target="_blank">
                   <img class="img-c" src="linkedin-circle.png">
                 </a>
               </li>
               <li>
-                <a target="_blank" href="mailto:toyeshsainik@gmail.com">
+                <a target="_blank" href="mailto:gaurav.02116403220@ipu.ac.in">
                   <img class="img-c" src="mail-circle.png">
                 </a>
               </li>
             </ul>
+            <a href="./gaurav-verma.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/Yuvraj.jpg" />
             </div>
             <h3 class="title">yuvraj chaturvedi</h3>
-            <a href="" class="link">project link</a>
             <ul class="social">
               <li>
-                <a href="https://github.com/Manik-Aggarwal" target="_blank">
+                <a href="github.com/Yuvraj004" target="_blank">
                   <img src="github-circle.png" class="img-c">
                 </a>
               </li>
               <li>
-                <a href="https://linkedin.com/in/manik-aggarwal-640110188" target="_blank">
+                <a href="https://www.linkedin.com/in/yuvraj-chaturvedi-a0773b22a" target="_blank">
                   <img class="img-c" src="linkedin-circle.png">
                 </a>
               </li>
               <li>
-                <a target="_blank" href="mailto:manikaggarwal993@gmail.com">
+                <a target="_blank" href="mailto:yuvrajchat@gmail.com">
                   <img class="img-c" src="mail-circle.png">
                 </a>
               </li>
             </ul>
+            <a href="Yuvraj.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+        <!-- <div class="col-md-3 col-sm-6">
+          <div class="our-team flex-column">
             <div class="pic">
               <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
             </div>
@@ -309,34 +259,34 @@
               </li>
             </ul>
           </div>
-        </div>
+        </div> -->
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
-              <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
+              <img src="students-img/subhangi.jpg" />
             </div>
             <h3 class="title">Subhangi dhasmana</h3>
-            <a href="" class="link">project link</a>
             <ul class="social">
               <li>
-                <a href="https://github.com/SakshamVarma" target="_blank">
+                <a href="https://github.com/SUBHANGI-DHASMANA" target="_blank">
                   <img src="github-circle.png" class="img-c">
                 </a>
               </li>
               <li>
-                <a target="_blank" href="https://www.linkedin.com/in/saksham-varma-45a218209"><img
+                <a target="_blank" href="https://www.linkedin.com/in/subhangi-dhasmana-04a911235/"><img
                     src="linkedin-circle.png" class="img-c"></a>
               </li>
               <li>
-                <a target="_blank" href="mailto:saksham.varma02@gmail.com">
+                <a target="_blank" href="mailto:subhangi788@gmail.com">
                   <img class="img-c" src="mail-circle.png">
                 </a>
               </li>
             </ul>
+            <a href="./subhangi-dhasmana.php" class="view-profile-link my-2">View profile</a>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+        <!-- <div class="col-md-3 col-sm-6">
+          <div class="our-team flex-column">
             <div class="pic">
               <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
             </div>
@@ -361,7 +311,7 @@
           </div>
         </div>
         <div class="col-md-3 col-sm-6">
-          <div class="our-team">
+          <div class="our-team flex-column">
             <div class="pic">
               <img src="http://www.ipu.ac.in/data0/new-website-images/rahulsir.jpg" />
             </div>
@@ -384,7 +334,7 @@
               </li>
             </ul>
           </div>
-        </div>
+        </div> -->
 
       </div>
     </div>
@@ -392,4 +342,3 @@
 </div>
 
   <?php include 'sdcusict2022footer.php' ?>
-</body>
